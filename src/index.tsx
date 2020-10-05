21a0b415e060720892cd89f73e5f65dc9be17c3c
Jacky Mint <xzees@users.noreply.github.com>
2020-10-05 22:22:34 +0700

    add Mater

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import * as React from "react";
+import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 import App from "./App";
 
-import "./styles.css";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import { MuiThemeProvider, lightBaseTheme } from "material-ui/styles";
 
+const lightMuiTheme = getMuiTheme(lightBaseTheme);
 const store = configureStore();
 
 const Root = () => (
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider muiTheme={lightMuiTheme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>
 );
 
