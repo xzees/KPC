11480c693ab78ef09fd3d7168a309f42ffd916a6
Jacky Mint <xzees@users.noreply.github.com>
2020-10-05 21:25:56 +0700

    set store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import { createStore, combineReducers } from "redux";
+
+import { todoReducer } from "./todo/reducers";
+
+const rootReducer = combineReducers({
+  todoReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default function configureStore() {
+  const store = createStore(rootReducer);
+
+  return store;
+}
